55eeb94203f4f509a189b3cca828ae9715a072e6
Deepanshu <deepanshujangra33233@gmail.com>
2024-06-01 01:08:31 +0530

    active side-bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
+"use client";
 import Tabs from "@/components/common/Tabs";
 import Profile from "@/components/homepage/Profile";
+import { useState } from "react";
 
 export default function Home() {
+  const [activeTab, setActiveTab] = useState("profile");
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="flex container 2xl:max-w-[1140px] px-3 mx-auto justify-between">
         <Profile />
         {/* <ParallaxCircles /> */}
-        <Tabs />
+        <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
       </div>
     </div>
   );
