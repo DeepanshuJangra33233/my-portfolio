de22b95c0cf977224656b436bee57a60a43db277
Aashi Sheoran <sheoranaashi0@gmail.com>
2024-06-01 00:34:34 +0530

    add profile

--- src/components/homepage/Profile.jsx
+++ src/components/homepage/Profile.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <>
+      <div>
+        <h2 className="text-[#ff014f] text-center">Deepanshu</h2>
+        <div className="text-[#ff014f] rounded-[10px] bg-card-gradient  shadow-box-shadow p-5">
+          <p>Frontend Dev</p>
+        </div>
+      </div>
+      ;
+    </>
+  );
 };
 
 export default Profile;
