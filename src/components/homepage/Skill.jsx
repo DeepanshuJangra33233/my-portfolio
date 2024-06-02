b54342f15c319059c318a2986f31c37aeeaf17ae
Deepanshu <deepanshujangra33233@gmail.com>
2024-06-02 17:02:52 +0530

    accept-inocming-dev

--- src/components/homepage/Skill.jsx
+++ src/components/homepage/Skill.jsx
@@ -5,9 +5,26 @@ import Image from "next/image";
 const Skill = () => {
   return (
     <>
-      <h3 className="text-base">PRIMARY SKILLS ON</h3>
-      <div className="flex items-center gap-5 mt-10 sm:p-3 flex-wrap">
+      <h3 className="text-base font-medium">PRIMARY SKILLS ON</h3>
+      <div className="flex items-center gap-5 mt-5 sm:p-3 flex-wrap">
         {SkillLogos.primarySkill.map((obj, index) => (
+          <div
+            key={index}
+            className="rounded-[10px] bg-card-gradient shadow-box-shadow p-[20px] flex items-center justify-center size-[62px] sm:size-[71px]"
+          >
+            <Image
+              className="w-full"
+              width={50}
+              height={50}
+              src={obj}
+              alt="logo"
+            />
+          </div>
+        ))}
+      </div>
+      <h3 className="text-base mt-10 font-medium">SECONDARY SKILLS ON</h3>
+      <div className="flex items-center gap-5 mt-5 sm:p-3 flex-wrap">
+        {SkillLogos.secondarySkill.map((obj, index) => (
           <div
             key={index}
             className="rounded-[10px] bg-card-gradient shadow-box-shadow p-[20px] flex items-center justify-center size-[62px] sm:size-[71px]"
