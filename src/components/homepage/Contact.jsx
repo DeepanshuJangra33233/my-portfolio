8b82db04f4f3dd8e7ea929cea1c44b4911e945f7
Aashi Sheoran <sheoranaashi0@gmail.com>
2024-06-02 17:05:10 +0530

    add from

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className=" flex gap-3">
+        <div className="">
+          <label
+            htmlFor=""
+            className=" text-[#a0a8b3] text-[12px] font-medium uppercase "
+          >
+            Your Name
+          </label>
+          <input
+            className=" bg-[#191b1e] px-[15px] py-[15px] rounded-[6px] mt-[15px]"
+            type="text"
+          />
+        </div>
+        <div className="">
+          <label
+            htmlFor=""
+            className=" text-[#a0a8b3] text-[12px] font-medium uppercase "
+          >
+            Phone Number
+          </label>
+          <input
+            className=" bg-[#191b1e] px-[15px] py-[15px] rounded-[6px] mt-[15px]"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="">
+        <label
+          htmlFor=""
+          className=" text-[#a0a8b3] text-[12px] font-medium uppercase text-start"
+        >
+          Email
+        </label>
+        <input
+          className=" bg-[#191b1e] px-[15px] py-[15px] rounded-[6px] mt-[15px]"
+          type="text"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
